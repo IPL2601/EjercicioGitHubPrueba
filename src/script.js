a8af4291c849bd55a9a0746ec554b809fa8ff06c
IPL2601 <ivanpelora2601@gmail.com>
2023-11-12 21:45:38 +0100

    Inicio

---
+++ src/script.js
@@ -0,0 +1,10 @@
+{
+    const boton = document.getElementById("boton1");
+    const color = document.getElementById("color");
+    const nombre = document.getElementById("nombre");
+
+    function cambiar() {
+        
+    }
+    boton.addEventListener("click", cambiar);
+}
